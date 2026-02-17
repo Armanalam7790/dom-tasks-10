def1290432fcfb607590a8c9d5e621f8264734b0
Armanalam7790 <alamarman7790@gmail.com>
2026-02-17 09:21:23 +0530

    task3 done

---
+++ task3/index.js
@@ -0,0 +1,13 @@
+var box = document.querySelector('.box')
+var btn = document.querySelector('button')
+
+btn.addEventListener('click', function() {
+
+    if (box.style.display === "none") {
+        box.style.display = "block"
+    } else {
+        box.style.display = "none"
+    }
+
+})
+
---
+++ task3/test.md
@@ -0,0 +1,12 @@
+## Task 3: Show and Hide a Box
+**Create:**
+- One button  
+- One box with text inside it  
+
+**When button is clicked:**
+- If box is visible → hide the box  
+- If box is hidden → show the box again  
+
+The box should not be deleted, only hidden.
+
+---
